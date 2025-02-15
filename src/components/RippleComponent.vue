e912934441dde34f8e0fa41c28dc758fea8ef782
HasanAbbadi <hasanabady55@gmail.com>
2025-02-15 18:20:30 +0200

    Refactored switch component

---
+++ src/components/RippleComponent.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isPointerDown = ref<boolean>(false)
+const ripple = ref<HTMLElement | null>(null)
+const rippleEffect = ref<HTMLElement | null>(null)
+
+const emit = defineEmits(['click'])
+
+const onClick = (e: MouseEvent) => {
+  emit('click')
+  if (ripple.value && rippleEffect.value) {
+    if (!isPointerDown.value) {
+      startRipple(e)
+      setTimeout(() => {
+        stopRipple()
+      }, 300)
+    }
+  }
+}
+
+const startRipple = (e: MouseEvent) => {
+  if (!ripple.value || !rippleEffect.value) return
+  isPointerDown.value = true
+  const rect = ripple.value.getBoundingClientRect()
+  const size = Math.max(rect.width, rect.height)
+  const x = e.clientX - rect.left - size / 2
+  const y = e.clientY - rect.top - size / 2
+
+  rippleEffect.value.style.width = rippleEffect.value.style.height = `${size}px`
+  rippleEffect.value.style.left = `${x}px`
+  rippleEffect.value.style.top = `${y}px`
+
+  rippleEffect.value.classList.remove('fade')
+  rippleEffect.value.classList.add('active')
+}
+
+const stopRipple = () => {
+  if (!rippleEffect.value) return
+  isPointerDown.value = false
+  rippleEffect.value.classList.add('fade')
+  rippleEffect.value.classList.remove('active')
+}
+</script>
+
+<template>
+  <div
+    class="ripple-wrapper"
+    @click.stop="onClick"
+    @pointerdown="startRipple"
+    @pointerup="stopRipple"
+    @pointerleave="stopRipple"
+    @pointercancel="stopRipple"
+  >
+    <span class="ripple" ref="ripple">
+      <span class="ripple-effect fade" ref="rippleEffect"></span>
+    </span>
+    <div class="body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.ripple-wrapper {
+  position: relative;
+  width: 100%;
+}
+
+.ripple {
+  position: absolute;
+  inset: 0;
+  overflow: hidden;
+  border-radius: inherit;
+  pointer-events: none;
+}
+
+.ripple-effect {
+  position: absolute;
+  background: radial-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4));
+  border-radius: 50%;
+  transform: scale(0);
+  opacity: 0.6;
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
+}
+
+.ripple-effect.active {
+  transform: scale(4);
+}
+
+.ripple-effect.fade {
+  transform: scale(0);
+  opacity: 0;
+}
+</style>
--- src/components/SwitchComponent.vue
+++ src/components/SwitchComponent.vue
@@ -1,73 +1,27 @@
 <script setup lang="ts">
-import { defineModel, ref } from 'vue'
+import { defineModel } from 'vue'
+import RippleComponent from '@/components/RippleComponent.vue'
 
 const state = defineModel<boolean>()
-const isPointerDown = ref<boolean>(false)
-const ripple = ref<HTMLElement | null>(null)
-const rippleEffect = ref<HTMLElement | null>(null)
 
 const updateState = () => {
   state.value = !state.value
 }
-
-const onClick = (e: MouseEvent) => {
-  updateState()
-
-  if (ripple.value && rippleEffect.value) {
-    if (!isPointerDown.value) {
-      startRipple(e)
-      setTimeout(() => {
-        stopRipple()
-      }, 300)
-    }
-  }
-}
-
-const startRipple = (e: MouseEvent) => {
-  if (!ripple.value || !rippleEffect.value) return
-  isPointerDown.value = true
-  const rect = ripple.value.getBoundingClientRect()
-  const size = Math.max(rect.width, rect.height)
-  const x = e.clientX - rect.left - size / 2
-  const y = e.clientY - rect.top - size / 2
-
-  rippleEffect.value.style.width = rippleEffect.value.style.height = `${size}px`
-  rippleEffect.value.style.left = `${x}px`
-  rippleEffect.value.style.top = `${y}px`
-
-  rippleEffect.value.classList.remove('fade')
-  rippleEffect.value.classList.add('active')
-}
-
-const stopRipple = () => {
-  if (!rippleEffect.value) return
-  isPointerDown.value = false
-  rippleEffect.value.classList.add('fade')
-  rippleEffect.value.classList.remove('active')
-}
 </script>
 
 <template>
-  <div
-    class="switch-component"
-    @click.stop="onClick"
-    @pointerdown="startRipple"
-    @pointerup="stopRipple"
-    @pointerleave="stopRipple"
-    @pointercancel="stopRipple"
-  >
-    <span class="ripple" ref="ripple">
-      <span class="ripple-effect fade" ref="rippleEffect"></span>
-    </span>
-    <div class="body">
-      <slot name="body-content" />
-    </div>
-    <div class="switch" @click.stop="updateState" :class="{ checked: state }">
-      <div class="switch-handle">
-        <slot name="handle-content" />
+  <RippleComponent>
+    <div class="switch-component" @click.stop="updateState">
+      <div class="body">
+        <slot name="body-content" />
+      </div>
+      <div class="switch" @click.stop="updateState" :class="{ checked: state }">
+        <div class="switch-handle">
+          <slot name="handle-content" />
+        </div>
       </div>
     </div>
-  </div>
+  </RippleComponent>
 </template>
 
 <style>
@@ -145,34 +99,6 @@ p {
   width: calc(var(--radius) - 2px);
   height: calc(var(--radius) - 2px);
   color: var(--color-text);
-  opacity: 0;
-}
-
-.ripple {
-  position: absolute;
-  inset: 0;
-  overflow: hidden;
-  border-radius: inherit;
-  pointer-events: none;
-}
-
-.ripple-effect {
-  position: absolute;
-  background: radial-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4));
-  border-radius: 50%;
-  transform: scale(0);
-  opacity: 0.6;
-  transition:
-    opacity 0.3s ease,
-    transform 0.3s ease;
-}
-
-.ripple-effect.active {
-  transform: scale(4);
-}
-
-.ripple-effect.fade {
-  transform: scale(0);
   opacity: 0;
 }
 </style>
