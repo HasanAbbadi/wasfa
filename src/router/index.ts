3014a17c6037d5df2ef2ec51c9570c2508e2bfb6
HasanAbbadi <hasanabady55@gmail.com>
2025-03-30 14:07:58 +0300

    Attempt to fix the recipe page 404 error

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import PrintLayout from '@/layouts/PrintLayout.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/wasfa/'),
   routes: [
     {
       path: '/',
