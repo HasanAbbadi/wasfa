cbe54d1e73592eb3aacb197e57114fbe5426e2f0
HasanAbbadi <hasanabady55@gmail.com>
2025-03-30 14:12:55 +0300

    Switched to vercel instead of GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import PrintLayout from '@/layouts/PrintLayout.vue'
 
 const router = createRouter({
-  history: createWebHistory('/wasfa/'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
